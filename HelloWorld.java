911b98e55df66e385d82acfd551efad8910160d9
srilekhaduggi <78536354+srilekhaduggi@users.noreply.github.com>
2021-06-12 22:29:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,13 +6,13 @@ public class HelloWorld {
 
         // Creates a reader instance which takes
         // input from standard input - keyboard
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+
+        System.out.print("This is my first automated build file of the jenkins");
 
         // nextInt() reads the next integer from the keyboard
-        int number = reader.nextInt();
+        
 
         // println() prints the following line to the output screen
-        System.out.println("You entered: " + number);
+        System.out.println("Build done.....yahoooooo.........");
     }
 }
