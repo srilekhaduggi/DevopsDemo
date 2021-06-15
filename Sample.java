cdd52c0f8d1af33d1dbb17df38ee88dbec5806de
srilekhaduggi <78536354+srilekhaduggi@users.noreply.github.com>
2021-06-15 16:54:50 +0530

    Add files via upload
    created new file for the demo purpose

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class Sample{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+        System.out.println("Welcome to the world of jenkins");
+        System.out.println("Creatign a job with pipeline");
+     }
+}
